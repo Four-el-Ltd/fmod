73ab1aa9ecf616c391be11998044542b33369085
ruslanllll <innaryslanllll@gmail.com>
2023-08-05 12:33:07 +0300

    Добавил вкладку в креативе

--- src/main/java/hlab/hmod/core/ExampleMod.java
+++ src/main/java/hlab/hmod/core/ExampleMod.java
@@ -1,15 +1,24 @@
 package hlab.hmod.core;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.wispforest.owo.itemgroup.OwoItemGroup;
+import io.wispforest.owo.itemgroup.Icon;
+import io.wispforest.owo.registration.reflect.FieldRegistrationHandler;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("hmod-core");
+	public static final Logger LOGGER = LoggerFactory.getLogger("hmod-core");
+	public static final OwoItemGroup INSTRUMENTS_GROUP = OwoItemGroup.builder(new Identifier("hmod", "instruments"),
+			() -> Icon.of(new ItemStack(Items.IRON_PICKAXE))).build();
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +27,6 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		INSTRUMENTS_GROUP.initialize();
 	}
 }
---
+++ src/main/java/hlab/hmod/core/items/ModItems.java
@@ -0,0 +1,7 @@
+package hlab.hmod.core.items;
+
+import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
+
+public class ModItems implements ItemRegistryContainer {
+
+}
