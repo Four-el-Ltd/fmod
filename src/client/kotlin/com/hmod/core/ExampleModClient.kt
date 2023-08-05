88b18e19a292ae4d106848af82e69ad16dddd6cd
ruslanllll <innaryslanllll@gmail.com>
2023-08-05 10:34:24 +0300

    Хуй

--- src/client/kotlin/com/hmod/core/ExampleModClient.kt
+++
@@ -1,9 +0,0 @@
-package com.hmod.core
-
-import net.fabricmc.api.ClientModInitializer
-
-object ExampleModClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
