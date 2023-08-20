09e1f35692a7cb7bf6d5ed31bbf0003542fc147c
Ruslanllll <innaryslanllll@gmail.com>
2023-08-20 19:43:26 +0300

    hotfix

--- src/client/java/hlab/hmod/core/ExampleModClient.java
+++ src/client/java/hlab/hmod/core/ExampleModClient.java
@@ -7,8 +7,10 @@ public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 
-		SpecialModelLoaderEvents.LOAD_SCOPE
-				.register(location -> ExampleMod.MODID.equals(location.getNamespace()));
+		/*
+		 * SpecialModelLoaderEvents.LOAD_SCOPE
+		 * .register(location -> ExampleMod.MODID.equals(location.getNamespace()));
+		 */
 
 		// This entrypoint is suitable for setting up client-specific logic, such as
 		// rendering.
