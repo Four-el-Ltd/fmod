1bd41b5af60101b35a9e5c6e212993b0cf8fd2ed
Ruslanllll <innaryslanllll@gmail.com>
2023-09-08 01:40:02 +0300

    test

---
+++ src/main/java/hlab/hmod/core/network/ModPackets.java
@@ -0,0 +1,14 @@
+package hlab.hmod.core.network;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+
+public class ModPackets {
+  public static void registerS2CPackets() {
+
+  }
+
+  public static void registerC2SPackets() {
+
+  }
+}
--- src/main/java/hlab/hmod/core/network/SyncHandler.java
+++
@@ -1,9 +0,0 @@
-package hlab.hmod.core.network;
-
-import hlab.hmod.core.ExampleMod;
-import net.minecraft.util.Identifier;
-
-public class SyncHandler {
-  public static final Identifier MIND_SYNC_ID = new Identifier(ExampleMod.MODID, "mind_sync");
-  
-}
