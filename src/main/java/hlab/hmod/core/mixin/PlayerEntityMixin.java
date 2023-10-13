0a3a0a51ad4f554bae06a41f06047127600256ee
Ruslanllll <innaryslanllll@gmail.com>
2023-10-13 21:13:01 +0300

    Вроде доделал нетворкинг

--- src/main/java/hlab/hmod/core/mixin/PlayerEntityMixin.java
+++ src/main/java/hlab/hmod/core/mixin/PlayerEntityMixin.java
@@ -5,12 +5,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import hlab.hmod.core.network.ModPackets;
 import hlab.hmod.core.util.SanityManager;
 import hlab.hmod.core.util.SanityManagerAccesor;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
@@ -40,6 +45,9 @@ public abstract class PlayerEntityMixin extends LivingEntity implements SanityMa
   public void hmod$UpdateSanity(CallbackInfo ci) {
     PlayerEntity player = (PlayerEntity) (Object) this;
     this.sanityManager.update(player);
+    ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+    ServerPlayNetworking.send(serverPlayer, ModPackets.SANITY_S2C_SYNC,
+        (PacketByteBuf) (PacketByteBufs.create().writeInt(this.sanityManager.getSanityLevel())));
   }
 
 }
--- src/main/java/hlab/hmod/core/network/packets/SyncSanityS2C.java
+++ src/main/java/hlab/hmod/core/network/packets/SyncSanityS2C.java
@@ -1,5 +1,7 @@
 package hlab.hmod.core.network.packets;
 
+import hlab.hmod.core.mixin.PlayerEntityMixin;
+import hlab.hmod.core.util.SanityManager;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
@@ -11,6 +13,8 @@ public class SyncSanityS2C {
   public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf,
       PacketSender responseSender) {
     int sanityLevel = buf.readInt();
-
+    PlayerEntity a = (PlayerEntity) client.player;
+    SanityManager b = ((PlayerEntityMixin) (Object) a).getSanityManager();
+    b.setSanity(sanityLevel);
   }
 }
