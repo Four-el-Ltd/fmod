2d99016d1532dc25e0a07f58ef0635e72859403f
Ruslanllll <innaryslanllll@gmail.com>
2023-10-14 10:42:50 +0300

    Продолжаю фокусы с дженериками

--- src/main/java/hlab/hmod/core/config/ConfigValue.java
+++ src/main/java/hlab/hmod/core/config/ConfigValue.java
@@ -2,9 +2,19 @@ package hlab.hmod.core.config;
 
 public class ConfigValue<T> {
   private T value;
+  private Class<T> valueType;
+
+  ConfigValue(T value, Class<T> valueType) {
+    this.value = value;
+    this.valueType = valueType;
+  }
 
   public T getValue() {
     return value;
+  }
+
+  public Class<T> getValueType() {
+    return this.valueType;
   }
 
   public void setValue(T newValue) {
